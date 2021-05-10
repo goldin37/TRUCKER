c2c8da483b6234e2e68a62daa89dbde5cefdc04a
playnauts <82129220+playnauts@users.noreply.github.com>
2021-05-10 17:18:51 +0900

    DeliveryOrderDB 복구

--- Eclipse Project/src/myUtil/DeliveryOrderDB.java
+++ Eclipse Project/src/myUtil/DeliveryOrderDB.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -15,12 +16,12 @@ import javax.sql.DataSource;
 public class DeliveryOrderDB {
 	private static DeliveryOrderDB instance = new DeliveryOrderDB();
 
-	//객체 생성
+	//객체 ?��?��
 	public static DeliveryOrderDB getInstance() {
 		return instance;
 	}
 	
-	//DB 접속
+	//DB ?���?
 	public Connection getConnection() throws Exception{
 		Context ctx=new InitialContext();
 		DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/oracle");
@@ -28,7 +29,7 @@ public class DeliveryOrderDB {
 	}
 	
 	public int createOrder(String truck_type, String cargo_type, String cargo_weight, String cargo_help, String cargo_spec, String from_where, String from_spec, String depart_time, String to_where, String to_spec, String distance, String time, String ETA, String recommend_cost, String fix_cost, String customer_name, String customer_telephone) throws Exception{
-		int re = 0; //order_id
+		int re = 0; //order_id�?
 
 		Connection con = null;
 		Statement stmt = null;
@@ -52,17 +53,17 @@ public class DeliveryOrderDB {
 				create_order_id = 1;
 			}
 			
-			//접속 종료
+			//?��?�� ?��?��
 			rs.close();
 			stmt.close();
 			con.close();
 			
-			//조회 성공 메시지
+			//?��?�� ?���? 메시�?
 			System.out.println("최대 order_id : " + max_order_id);
 			System.out.println("삽입 order_id : " + create_order_id);
-			System.out.println("DB 조회 성공");
+			System.out.println("DB 조회 ?���?");
 		} catch(Exception e) {
-			System.out.println("DB 조회 실패");
+			System.out.println("DB 조회 ?��?��");
 			e.printStackTrace();
 		}
 		//주문 insert
@@ -85,20 +86,20 @@ public class DeliveryOrderDB {
 			pstmt.setString(11, to_spec);
 			pstmt.setInt(12, Integer.parseInt(distance));
 			pstmt.setString(13, time);
-			pstmt.setTimestamp(14, Timestamp.valueOf(LocalDateTime.parse(ETA)));
-			pstmt.setInt(15, Integer.parseInt(fix_cost));
+			pstmt.setString(14, ETA);
+			pstmt.setInt(15, Integer.parseInt(recommend_cost));
 			pstmt.setInt(16, Integer.parseInt(fix_cost));
 			pstmt.setString(17, customer_name);
 			pstmt.setString(18, customer_telephone);
 			pstmt.setTimestamp(19, Timestamp.valueOf(LocalDateTime.now()));
 			pstmt.executeUpdate();
 			
-			//접속 종료
+			//?��?�� 종료
 			rs.close();
 			pstmt.close();
 			con.close();
 
-			//성공 메시지
+			//?���? 메시�?
 			System.out.println("주문 성공");
 			re = create_order_id;
 		} catch(Exception e){
@@ -106,8 +107,126 @@ public class DeliveryOrderDB {
 			e.printStackTrace();
 		}
 		
-		//성공하면 order_id 리턴
+		//?��?��?�� order_id 리턴
 		return re;
 	}
-
+	
+	
+	public ArrayList<DeliveryOrder> listOrder(String pageNumber){
+		//오더리스트에 추가하는 메소드
+		
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		ResultSet pageSet = null; // 페이지를 받기 위해
+		int dbCount = 0; // 페이지 번호의 개수를 받기 위한 변수
+		int absolutePage = 1; // 출력할 페이지
+		ArrayList<DeliveryOrder> orderList = new ArrayList<DeliveryOrder>();
+		
+		try {
+			con = getConnection();
+			stmt = con.createStatement();
+			String sql = "select count(order_id) from delivery_order";
+			pageSet  = stmt.executeQuery(sql);
+			
+			if(pageSet.next()) {
+				dbCount = pageSet.getInt(1);
+				pageSet.close();
+				stmt.close();
+			}
+			
+			if(dbCount % DeliveryOrder.pageSize ==0 ) { //페이지 개수를 알려주기 위해
+				DeliveryOrder.pageCount  = dbCount / DeliveryOrder.pageSize;
+			}else {
+				DeliveryOrder.pageCount = dbCount / DeliveryOrder.pageSize + 1;
+			}
+			
+			stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			sql = "select * from delivery_order";
+			rs = stmt.executeQuery(sql);
+			if(rs.next()) {
+				rs.absolute(absolutePage);
+				int count = 0;
+				while(count<DeliveryOrder.pageSize) {
+					DeliveryOrder deliveryorder = new DeliveryOrder();
+					
+					deliveryorder.setTruck_type(rs.getString(1));
+					deliveryorder.setCargo_type(rs.getString(2));
+					deliveryorder.setCargo_weight(rs.getInt(3));
+					deliveryorder.setCargo_help(rs.getString(4));
+					deliveryorder.setFrom(rs.getString(5));
+					deliveryorder.setTo(rs.getString(6));
+					deliveryorder.setDistance(rs.getInt(7));
+					deliveryorder.setTime(rs.getString(8));
+					deliveryorder.setETA(rs.getString(9));
+					deliveryorder.setDepart_time(rs.getTimestamp(10));
+					deliveryorder.setRecommend_cost(rs.getInt(11));
+					
+					orderList.add(deliveryorder);
+					if(rs.isLast()) {
+						break;
+					}else {
+						rs.next();
+					}
+					count++;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs!=null) rs.close();
+				if(stmt!=null) stmt.close();
+				if(con !=null) con.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return orderList;
+	}
+	public DeliveryOrder getOrder(int order_id) throws Exception{
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs =null;
+		String sql = "";
+		DeliveryOrder deliveryorder =null;
+		
+		try {
+			con = getConnection();
+			sql = "select * from delivery_order where order_id =?";
+			pstmt =con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				deliveryorder = new DeliveryOrder();
+				
+				deliveryorder.setTruck_type(rs.getString(1));
+				deliveryorder.setCargo_type(rs.getString(2));
+				deliveryorder.setCargo_weight(rs.getInt(3));
+				deliveryorder.setCargo_help(rs.getString(4));
+				deliveryorder.setCargo_spec(rs.getString(5));
+				deliveryorder.setFrom(rs.getString(6));
+				deliveryorder.setTo(rs.getString(7));
+				deliveryorder.setDistance(rs.getInt(8));
+				deliveryorder.setTime(rs.getString(9));
+				deliveryorder.setETA(rs.getString(10));
+				deliveryorder.setDepart_time(rs.getTimestamp(11));
+				deliveryorder.setRecommend_cost(rs.getInt(12));
+				deliveryorder.setCustomer_name(rs.getString(13));
+				deliveryorder.setCustomer_telephone(rs.getString(14));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(con!=null) con.close();
+				if(pstmt != null) pstmt.close();
+				if(rs != null) rs.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return deliveryorder;
+	}
 }
